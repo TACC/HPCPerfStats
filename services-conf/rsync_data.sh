36058e8b157ad20b54d73a19df21827cc9888cc6
Stephen Lien Harrell <stephen@teknikal.org>
2025-10-15 20:02:43 -0500

    Enhance rsync_data.sh with explanatory comments
    Added comments to clarify the purpose of rsync commands.

--- services-conf/rsync_data.sh
+++ services-conf/rsync_data.sh
@@ -2,10 +2,11 @@
 set -x
 while true
 do
-    /usr/bin/rsync -avq -e 'ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no' sharrell@staff.stampede3.tacc.utexas.edu:/home1/01623/sharrell/s3_acct_logs/* /hpcperfstats/accounting/
-    /usr/bin/rsync -avpq --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r -e 'ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no' /hpcperfstats/daily_archive/*.gz sharrell@ranch.tacc.utexas.edu:/stornext/ranch_01/ranch/projects/tacc_stats/stampede3/tgz_archive/
-    /usr/bin/rsync -avpq --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r -e 'ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no' /hpcperfstats/accounting/* sharrell@ranch.tacc.utexas.edu:/stornext/ranch_01/ranch/projects/tacc_stats/stampede3/xms-accounting
-    /usr/bin/rsync -avpq -e 'ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no' /hpcperfstatslog/*.gz sharrell@ranch.tacc.utexas.edu:/stornext/ranch_01/ranch/projects/tacc_stats/stampede3/node_logs/
-    # Wait 12 hours, then do it again!
+# The first command copies the accounting files into the container.
+# The second command moves all archived node-level data to a tape archive.
+    /usr/bin/rsync -avq -e 'ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no' hpcperf@staff.stampede3.tacc.utexas.edu:/home1/01623/sharrell/s3_acct_logs/* /hpcperfstats/accounting/
+    /usr/bin/rsync -avpq --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r -e 'ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no' /hpcperfstats/daily_archive/*.gz hpcperf@ranch.tacc.utexas.edu:/stornext/ranch_01/ranch/projects/tacc_stats/stampede3/tgz_archive/
+  
+
     sleep 43200
 done
