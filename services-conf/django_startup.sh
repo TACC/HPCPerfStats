2b1ec6f0d7d3a1ed31ef55521ff684acef818a2d
Stephen Lien Harrell <sharrell@tacc.utexas.edu>
2026-03-05 08:57:19 -0600

    Pull the thread count from the config file for worker numbers for gunicorn

--- services-conf/django_startup.sh
+++ services-conf/django_startup.sh
@@ -15,8 +15,12 @@ chown -R hpcperfstats:hpcperfstats /hpcperfstats/
 /usr/local/bin/python3 hpcperfstats/site/manage.py makemigrations
 /usr/local/bin/python3 hpcperfstats/site/manage.py migrate
 
-# gunicorn is the django web server - this is configued for a 40 core machine
+# determine thread count from conf_parser and set gunicorn workers = thread_count*2+1
+THREAD_COUNT=$(/usr/local/bin/python3 -c "from hpcperfstats import conf_parser; print(conf_parser.get_total_cores())")
+WORKERS=$((THREAD_COUNT * 2 + 1))
+
+# gunicorn is the django web server
 /usr/local/bin/gunicorn hpcperfstats.site.hpcperfstats_site.wsgi --bind 0.0.0.0:8000  \
   --env DJANGO_SETTINGS_MODULE=hpcperfstats.site.hpcperfstats_site.settings -u hpcperfstats \
-  --workers=40  --timeout 600 --preload --max-requests 100 --access-logfile - --error-logfile - 
+  --workers=${WORKERS} --timeout 600 --preload --max-requests 100 --access-logfile - --error-logfile - 
 
