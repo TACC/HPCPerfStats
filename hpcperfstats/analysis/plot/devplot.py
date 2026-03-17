6dd93155a0626aa17686393a1c162368a4bc7a35
Stephen Lien Harrell <sharrell@tacc.utexas.edu>
2026-03-17 07:04:18 -0500

    Add plot points to devplot and summaryplot

--- hpcperfstats/analysis/plot/devplot.py
+++ hpcperfstats/analysis/plot/devplot.py
@@ -55,22 +55,35 @@ class DevPlot:
     )
     plot.xaxis.formatter = tz_aware_bokeh_tick_formatter()
 
-    # Hover shows which line (host) and value; no legend (identify line by hovering).
-    plot.add_tools(HoverTool(
-        tooltips=[
-            ("host", "@host"),
-            ("time", "@time{%F %T}"),
-            (event, "@" + event),
-        ],
-        formatters={"@time": "datetime"},
-    ))
-
+    circle_renderers = []
     for h in self.host_list:
       source = ColumnDataSource(df[df.host == h])
       plot.add_glyph(
           source,
           Step(x="time", y=event, mode="before", line_color=self.hc[h]),
       )
+      circle = plot.circle(
+          x="time",
+          y=event,
+          source=source,
+          size=4,
+          color=self.hc[h],
+          alpha=0.9,
+      )
+      circle_renderers.append(circle)
+
+    # Hover shows which sample point (host) and value; no legend (identify line by hovering).
+    plot.add_tools(
+        HoverTool(
+            tooltips=[
+                ("host", "@host"),
+                ("time", "@time{%F %T}"),
+                (event, "@" + event),
+            ],
+            formatters={"@time": "datetime"},
+            renderers=circle_renderers,
+        )
+    )
     log.debug("time to plot %s: %s", event, time.time() - s)
     return plot
 
